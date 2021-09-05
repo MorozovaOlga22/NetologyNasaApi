ad72e01e247ac87a1c5fab729db96da8d81e707b
Olga Morozova <Morozova_Olga_22@mail.ru>
2021-09-05 19:55:24 +0300

    Video support added

--- src/main/java/ru/netology/Main.java
+++ src/main/java/ru/netology/Main.java
@@ -11,6 +11,8 @@ import org.json.simple.parser.ParseException;
 
 import java.io.File;
 import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.StandardCopyOption;
@@ -20,10 +22,11 @@ public class Main {
 
     public static void main(String[] args) {
         try (final CloseableHttpClient httpClient = createHttpClient()) {
-            final HttpGet requestUrl = new HttpGet(REMOTE_SERVICE_URI);
-            try (final CloseableHttpResponse responseUrl = httpClient.execute(requestUrl)) {
-                downloadFile(httpClient, responseUrl);
-            } catch (ParseException e) {
+            final HttpGet request = new HttpGet(REMOTE_SERVICE_URI);
+            try (final CloseableHttpResponse response = httpClient.execute(request)) {
+                final String url = getFileUrl(response);
+                downloadFile(httpClient, url);
+            } catch (ParseException | URISyntaxException e) {
                 e.printStackTrace();
             }
         } catch (IOException e) {
@@ -41,23 +44,39 @@ public class Main {
                 .build();
     }
 
-    private static void downloadFile(CloseableHttpClient httpClient, CloseableHttpResponse responseUrl) throws IOException, ParseException {
-        final String url = getFileUrl(responseUrl);
-        final String[] urlParts = url.split("/");
-        final String fileName = urlParts[urlParts.length - 1];
-
-        final HttpGet requestFile = new HttpGet(url);
-        try (final CloseableHttpResponse responseFile = httpClient.execute(requestFile)) {
-            final String pathToFile = "directoryForNasaFiles" + File.separator + fileName;
-            Files.copy(responseFile.getEntity().getContent(), new File(pathToFile).toPath(), StandardCopyOption.REPLACE_EXISTING);
-        }
-    }
-
-    private static String getFileUrl(CloseableHttpResponse response) throws IOException, ParseException {
+    private static String getFileUrl(CloseableHttpResponse response) throws IOException, ParseException, URISyntaxException {
         final JSONParser parser = new JSONParser();
 
         final String body = new String(response.getEntity().getContent().readAllBytes(), StandardCharsets.UTF_8);
         final JSONObject obj = (JSONObject) parser.parse(body);
-        return (String) obj.get("url");
+        final String url = (String) obj.get("url");
+        if ("video".equals(obj.get("media_type"))) {
+            return getYoutubePictureUrl(url);
+        }
+
+        return url;
+    }
+
+    private static String getYoutubePictureUrl(String urlToVideo) throws URISyntaxException {
+        final URI uri = new URI(urlToVideo);
+        final String[] uriParts = uri.getPath().split("/");
+        final String pathForPicture = uriParts[uriParts.length - 1];
+
+        return "https://img.youtube.com/vi/" + pathForPicture + "/hqdefault.jpg";
+    }
+
+    private static void downloadFile(CloseableHttpClient httpClient, String url) throws IOException, ParseException {
+        final String pathToFile = getPathToFile(url);
+
+        final HttpGet requestFile = new HttpGet(url);
+        try (final CloseableHttpResponse responseFile = httpClient.execute(requestFile)) {
+            Files.copy(responseFile.getEntity().getContent(), new File(pathToFile).toPath(), StandardCopyOption.REPLACE_EXISTING);
+        }
+    }
+
+    private static String getPathToFile(String url) {
+        final String[] urlParts = url.split("/");
+        final String fileName = urlParts[urlParts.length - 1];
+        return "directoryForNasaFiles" + File.separator + fileName;
     }
 }
